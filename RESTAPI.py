5522b2d8bba904cf3bc9826e65629550d1ecac72
Genzme <ziazarinr@gmail.com>
2025-12-10 21:36:44 +0700

    heheh bingung

--- RESTAPI.py
+++ RESTAPI.py
@@ -4,5 +4,12 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+class LangkahSatu(Resource):
+    def get(self):
+        return {"In Maintenance"}
+    
+api.add_resource(LangkahSatu, "/step1")
+
 if __name__ == "__main__":
     app.run(debug=True)
+
